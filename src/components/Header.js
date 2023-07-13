39cb57f88bd5395b6159367a8062eed50050f842
Prathamesh Ghorpade <ghorpadeprathamesh2411@gmail.com>
2023-07-13 17:57:35 +0530

    active class added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,12 @@ const Header = () => {
       <a href='#testimonial'>Testimonials</a>
       <a href='#service'>Services</a>
       <a href='#features'>Features</a>
-      <a href='#banner'>Home</a>
+      <a
+        href='#banner'
+        className='active'
+      >
+        Home
+      </a>
     </>
   );
 };
